326d741e4a2e756214ccf28bae81f08403c44cf9
DanielAshley <dashley2@mail.usf.edu>
2014-02-26 11:32:09 -0500

    updated programs

---
+++ _C++/SpineBio2/SpineBio/stdafx.h
@@ -0,0 +1,11 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+#pragma once
+
+//#pragma comment( lib, "RTC3Dclient.lib" )
+#include "RTC3Dclient.h"
+#include "SockStream.h"
+#include <msclr/marshal_cppstd.h>
+//#include <sys/socket.h>
+// TODO: reference additional headers your program requires here
---
+++ _C++/SpineBio3/SpineBio/stdafx.h
@@ -0,0 +1,9 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+#pragma once
+
+#include "SockStream.h"
+#include <msclr/marshal_cppstd.h>
+//#include <sys/socket.h>
+// TODO: reference additional headers your program requires here
